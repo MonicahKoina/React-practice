42a353e5f4c45efc2661a3a26859869d242a4459
Monicah Koina <monicahkoina1@gmail.com>
2024-10-08 06:16:38 +0300

    toggle a boolean

--- src/Stringupdate.jsx
+++ src/Stringupdate.jsx
@@ -4,6 +4,7 @@ import  {useState} from 'react';
 function Stringupdate() {
     const[name, setName]=useState();
     const[age, setAge]=useState(0);
+    const[isemployed, setIsEmployed]=useState(false)
   return (
     <>
     <p>Name:{name}</p>
@@ -12,6 +13,9 @@ function Stringupdate() {
     })}>SET NAME</button>
     <p>Age:{age}</p>
     <button onClick={(()=>{setAge(age + 1)})}>Increment</button>
+    
+    <p>Is employed:{isemployed ? "Yes": "No"}</p>
+    <button onClick={(()=>{setIsEmployed(!isemployed)})}>Toggle button</button>
     </>
   )
 }

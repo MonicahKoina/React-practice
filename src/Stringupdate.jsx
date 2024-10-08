cbca8f29a936d6a0aaa2fbf78bfdc81528ac7450
Monicah Koina <monicahkoina1@gmail.com>
2024-10-08 06:04:44 +0300

    Increment a number

--- src/Stringupdate.jsx
+++ src/Stringupdate.jsx
@@ -3,12 +3,15 @@ import  {useState} from 'react';
 
 function Stringupdate() {
     const[name, setName]=useState();
+    const[age, setAge]=useState(0);
   return (
     <>
     <p>Name:{name}</p>
     <button onClick={(()=>{
         setName("Monicah");
     })}>SET NAME</button>
+    <p>Age:{age}</p>
+    <button onClick={(()=>{setAge(age + 1)})}>Increment</button>
     </>
   )
 }
